8dae573420e8f60528ec34d8e64125649f8ef9ff
Joe <jizhe7550@gmail.com>
2021-07-23 17:45:58 +1200

    creat init activity and service and dependencies

---
+++ app/src/main/java/com/eroadtest/eroadtest/SensorService.kt
@@ -0,0 +1,10 @@
+package com.eroadtest.eroadtest
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.lifecycle.LifecycleService
+
+class SensorService : LifecycleService() {
+
+}
